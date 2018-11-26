e9c52ee0cf3695f7d538113903e07f73fbc041dc
Ian Duffy <ian@ianduffy.ie>
2018-11-26 08:19:55 +0000

    Brew formula update for token-cli version v0.0.1

--- Formula/token-cli.rb
+++ Formula/token-cli.rb
@@ -3,9 +3,13 @@ class TokenCli < Formula
   homepage "https://github.com/imduffy15/token-cli"
   url "https://github.com/imduffy15/token-cli/releases/download/v0.0.1/token-cli_macOS_amd64.tar.gz"
   version "0.0.1"
-  sha256 "eb0d78d31aca90d539632d672eb761a4b2ea2cd78ea1a50eb33e8397e472d426"
+  sha256 "9bbd9f73aa8cc4bc114162fae3d16b128c33dfc438155e17279d4de1894b9cad"
 
   def install
     bin.install "token-cli"
+  end
+
+  test do
+    token-cli version
   end
 end
