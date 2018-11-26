86190d56b6db819c88191ca7ef7580f261dcd770
Ian Duffy <ian@ianduffy.ie>
2018-11-26 08:04:21 +0000

    Brew formula update for token-cli version v0.0.1

---
+++ Formula/token-cli.rb
@@ -0,0 +1,15 @@
+class TokenCli < Formula
+  desc "OpenID token generator"
+  homepage "https://github.com/imduffy15/token-cli"
+  url "https://github.com/imduffy15/token-cli/releases/download/v0.0.1/token-cli_macOS_amd64.tar.gz"
+  version "0.0.1"
+  sha256 "492b452d414e9a56689d8f3aafda950406e2a409e223f7286b7b129281f8f6e7"
+
+  def install
+    bin.install "token-cli"
+  end
+
+  test do
+    token-cli version
+  end
+end
