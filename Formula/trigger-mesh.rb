9b21ed3ca49ff6ff1be6b5afabba4e47edb53bce
Ian Duffy <duffy@magenta.lan>
2019-03-15 15:27:59 +0000

    fix

--- Formula/trigger-mesh.rb
+++ Formula/trigger-mesh.rb
@@ -6,7 +6,7 @@ class TriggerMesh < Formula
   sha256 "bfcd41fa67319b85e4c0a6e4ba0c0dce1f1179569b70871d6bb4ea9556d7c52c"
 
   def install
-    system "mv" "tm_osx" "tm"
+    system "mv", "tm_osx", "tm"
     bin.install "tm"
   end
 end
